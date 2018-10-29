8d35b96160511856f7e7ffc21007c431da7410ee
zia <ziamurad27@gmail.com>
2018-10-29 17:12:28 -0400

    final changes

---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,14 @@
+
+document.querySelector("h1").style.color="yellow";
+document.querySelector("h1").style.backgroundColor = "red";
+
+var h1= document.querySelector("h1")
+
+document.querySelector("#title").style.color="green";
+document.querySelector("#text-chunk").style.color="green";
+
+
+
+
+var name="avocato"
+alert (name)
