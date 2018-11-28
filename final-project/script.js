157be5435289a6f51f652b9774576faa8b3a6beb
zia <ziamurad27@gmail.com>
2018-11-28 18:20:52 -0500

    finalfinal

--- final-project/script.js
+++ final-project/script.js
@@ -47,26 +47,3 @@ function getRandomElement() {
     $('#randomise').hide()
 }
 
-
-/*
-function reloadPage() {
-    $('#clear-entry').reload();
-}
-
-    /*$('#reveal').html('randomElement')
-
-
-   /* 8 (ctn): update #total element
-    $('#total').html('$' + total)
-
-    // 9: clear #newEntry
-    $('#newEntry').val('')
-
-
-<img src="https://vignette.wikia.nocookie.net/nation/images/2/2e/Book_icon.png/revision/latest?cb=20090517180732" alt="book"> 
-
-    <img src="https://d30y9cdsu7xlg0.cloudfront.net/png/82812-200.png" alt="food"> 
-
-    <img src="https://melbournechapter.net/images/old-clipart-tv-icon-9.png" alt="tv">
-
-    */
