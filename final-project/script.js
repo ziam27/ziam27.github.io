275176ba3085b1c4c478df86289a303057595fe2
zia <ziamurad27@gmail.com>
2018-11-28 00:53:37 -0500

    fp1

--- final-project/script.js
+++ final-project/script.js
@@ -1,3 +1,10 @@
+$('document').ready(function() {
+    var options = { videoId: 'iGpuQ0ioPrM', start: 3 };
+    $('#wrapper').tubular(options);
+
+});
+
+
 $('#clickme').click(handleClick)
 
 var answers = []
