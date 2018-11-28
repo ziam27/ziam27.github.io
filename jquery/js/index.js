c56f43d3d2491adc19c27964f7d9e19647e9e4f5
zia <ziamurad27@gmail.com>
2018-11-28 12:34:48 -0500

    sdasa

--- jquery/js/index.js
+++ jquery/js/index.js
@@ -2,4 +2,55 @@ $('document').ready(function() {
 	var options = { videoId: 'iGpuQ0ioPrM', start: 3 };
 	$('#wrapper').tubular(options);
 
-});
+}
+
+$('#clickme').click(handleClick)
+
+var answers = []
+
+function handleClick(event) {
+    event.preventDefault()
+    
+    var newItem = $('#item').val()
+
+   
+    if (newItem.length === 0) {
+        alert('Enter one of your choices,fool ')
+    } else {
+        // Add new item to array
+        answers.push(newItem)
+
+        console.log(answers)
+
+        appendItem(newItem)
+
+        $('#item')
+            .focus()
+            .val('')
+    }
+}
+
+function appendItem(item) {
+    $('#list').append('<li>' + item + '</li>')
+}
+
+$(document).on('click', 'li', handleRemove)
+
+function handleRemove() {
+    $(this).remove() 
+}
+
+$('#randomise').click(getRandomElement)
+
+function getRandomElement() {
+    var randomNum = Math.floor(Math.random() * answers.length)
+    var randomElement = answers[randomNum]
+    console.log(answers[randomNum])
+
+    // insert randomElement into HTML
+    $('#final').append('<h4>' + randomElement + '</h4>')
+    // $('#randomise').attr('disabled', true)
+    $('#randomise').hide()
+}
+
+
